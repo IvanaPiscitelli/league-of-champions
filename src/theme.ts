57c2e19e413d53a5e910f6f5906645de2f81666e
Ivana Piscitelli <ivanap.ipiscitelli@gmail.com>
2023-06-21 16:15:54 +0200

    customized chakra ui theme

--- src/theme.ts
+++ src/theme.ts
@@ -9,6 +9,22 @@ const config: ThemeConfig = {
 };
 
 // 3. extend the theme and import in main.tsx
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config,
+  colors: {
+    gray: {
+      50: "#ebf7f9",
+      100: "#c9dbe9",
+      200: "#a6c4db",
+      300: "#81accd",
+      400: "#5f95c1",
+      500: "#487ca7",
+      600: "#386082",
+      700: "#29445b",
+      800: "#182a37",
+      900: "#060e14",
+    },
+  },
+});
 
 export default theme;
