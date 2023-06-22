68e91d33fe580082968908f4a2a3ddccee135ae4
Ivana Piscitelli <ivanap.ipiscitelli@gmail.com>
2023-06-22 11:45:42 +0200

    changed icon switch mode

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,12 +1,15 @@
-import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+import { HStack, Switch, useColorMode } from "@chakra-ui/react";
+import { IoMdMoon } from "react-icons/io";
+import { BsSun } from "react-icons/bs";
 
 const ColorModeSwitch = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
     <HStack>
-      <Switch colorScheme="blue" isChecked={colorMode === "dark"} onChange={toggleColorMode} />
-      <Text whiteSpace="nowrap">Dark Mode</Text>
+      <IoMdMoon />
+      <Switch colorScheme="blue" isChecked={colorMode !== "dark"} onChange={toggleColorMode} />
+      <BsSun />
     </HStack>
   );
 };
